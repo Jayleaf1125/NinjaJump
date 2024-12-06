ed23f1bad199498d6500aa3fcec34febc176781b
Jason Padilla <84742412+Jayleaf1125@users.noreply.github.com>
2024-12-06 14:38:24 -0500

    Implemented Player Death upon touching a spike

--- Assets/Scripts/Death.cs
+++ Assets/Scripts/Death.cs
@@ -5,33 +5,11 @@ using UnityEngine;
 
 public class Death : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    private void OnCollisionEnter2D(Collision2D other)
-    {
-        if(other.gameObject.tag == "Spike")
-        {
-            Debug.Log(10);
-        }
-        
-        
-    }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Spike")
+        if (collision.gameObject.CompareTag("Spike")) 
         {
-            Debug.Log(10);
+            Destroy(this.gameObject);
         }
     }
 
